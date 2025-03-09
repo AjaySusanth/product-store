f022bfb8fa59ee324bca2d4f120380368c4b42af
Ajay Susanth <ajaysusanth10@gmail.com>
2025-03-09 14:10:44 +0530

    setting up rate limiting

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,28 @@
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import 'dotenv/config'
+
+export const aj = arcjet({
+    key:process.env.ARCJET_KEY,
+    characteristics: ["ip.src"], 
+    rules: [
+        // Shield protects your app from common attacks e.g. SQL injection
+        shield({ mode: "LIVE" }),
+        // Create a bot detection rule
+        detectBot({
+          mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+          // Block all bots except the following
+          allow: [
+            "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+          ],
+        }),
+
+        // rate limiting
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 30, // Refill 5 tokens per interval
+            interval: 5, // Refill every 10 seconds
+            capacity: 20, // Bucket capacity of 10 tokens
+          }),
+        ],
+})
+
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,64 @@
-import express from 'express'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import productRoutes from './routes/productRoutes.js'
-import { sql } from './config/db.js'
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("dev"))
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("Test response")
-})
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
 
-app.use("/api/products",productRoutes)
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    //Spoofed bots detection
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
+
+app.use("/api/products", productRoutes);
 
 async function initDB() {
-    try {
-        await sql`
+  try {
+    await sql`
         CREATE TABLE IF NOT EXISTS products(
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
@@ -34,15 +66,15 @@ async function initDB() {
         price DECIMAL(10,2) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
-        `
-        console.log("DB initialised")
-    } catch (error) {
-        console.error("Error initialising db",error)
-    }
+        `;
+    console.log("DB initialised");
+  } catch (error) {
+    console.error("Error initialising db", error);
+  }
 }
 
-initDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Server running")
-    })
-})
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running");
+  });
+});
